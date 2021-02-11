2958b62f2f32cb13f0bcf3b32852249fbec11f75
nivedmk <43902334+nivedmk@users.noreply.github.com>
2021-02-11 21:53:50 +0530

    mongoDB file creation

---
+++ src/db/mongoDB.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+    console.log("MongoDB database connection established successfully");
+})
