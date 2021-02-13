546b5b7aff633d996f1fe85d35bf338de3cb086d
nivedmk <43902334+nivedmk@users.noreply.github.com>
2021-02-13 19:56:09 +0530

    Auth features

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("./db/mongoDB");
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT | 4000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
